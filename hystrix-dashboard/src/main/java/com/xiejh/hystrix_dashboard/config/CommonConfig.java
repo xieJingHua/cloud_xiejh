14e42c8b6e6913a008495c36881c75c47d0b0f61
xjh <xiejinghua@bluemoon.com.cn>
2020-10-12 16:31:35 +0800

    update hystrix-dashboard监控

---
+++ hystrix-dashboard/src/main/java/com/xiejh/hystrix_dashboard/config/CommonConfig.java
@@ -0,0 +1,24 @@
+package com.xiejh.hystrix_dashboard.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author xiejh
+ * @Date 2020/10/12 16:17
+ **/
+@Configuration
+public class CommonConfig {
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
