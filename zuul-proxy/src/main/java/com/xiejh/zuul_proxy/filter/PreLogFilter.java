ef201a486802581c9e98b25044d32681acb4c652
xjh <xiejinghua@bluemoon.com.cn>
2020-10-13 13:24:50 +0800

    update zuul学习

---
+++ zuul-proxy/src/main/java/com/xiejh/zuul_proxy/filter/PreLogFilter.java
@@ -0,0 +1,47 @@
+package com.xiejh.zuul_proxy.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author xiejh
+ * @Date 2020/10/13 9:43
+ **/
+@Slf4j
+@Component
+public class PreLogFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 自定义的过滤器逻辑，当shouldFilter()返回true时会执行。
+     */
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        String host = request.getRemoteHost();
+        String method = request.getMethod();
+        String uri = request.getRequestURI();
+        log.info("Remote host:{},method:{},uri:{}", host, method, uri);
+        return null;
+    }
+}
