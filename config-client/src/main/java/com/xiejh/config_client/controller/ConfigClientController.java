9c1002463402fa3d239bc41ea22fa60a4d0a5be7
xjh <xiejinghua@bluemoon.com.cn>
2020-10-14 15:40:46 +0800

    update admin监控

---
+++ config-client/src/main/java/com/xiejh/config_client/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.xiejh.config_client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiejh
+ * @Date 2020/10/13 14:27
+ **/
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
