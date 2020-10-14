e77019435814e594c828949b9029385e9055667d
xjh <xiejinghua@bluemoon.com.cn>
2020-10-14 17:31:49 +0800

    update admin监控

--- admin-server/src/main/java/com/xiejh/admin_server/AdminServerApplication.java
+++ admin-server/src/main/java/com/xiejh/admin_server/AdminServerApplication.java
@@ -3,8 +3,11 @@ package com.xiejh.admin_server;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AdminServerApplication {
 
