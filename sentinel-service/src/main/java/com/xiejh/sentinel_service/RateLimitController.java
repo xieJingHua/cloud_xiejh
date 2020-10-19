ed699615e6615ef311d69bbc18e3c62f666e1b64
xieJingHua <xiejh888@gmail.com>
2020-10-19 21:21:08 +0800

    update sentinel限流

---
+++ sentinel-service/src/main/java/com/xiejh/sentinel_service/RateLimitController.java
@@ -0,0 +1,38 @@
+package com.xiejh.sentinel_service;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+
+/**
+ * 限流功能
+ *
+ * @author xiejh
+ * @Date 2020/10/16 17:20
+ **/
+@RestController
+@RequestMapping("/rateLimit")
+public class RateLimitController {
+
+    /**
+     * 按资源名称限流，需要指定限流处理逻辑
+     */
+    @GetMapping("/byResource")
+    @SentinelResource(value = "byResource",blockHandler = "handleException")
+    public CommonResult byResource() {
+        return new CommonResult("按资源名称限流", 200);
+    }
+
+    /**
+     * 按URL限流，有默认的限流处理逻辑
+     */
+    @GetMapping("/byUrl")
+    @SentinelResource(value = "byUrl",blockHandler = "handleException")
+    public CommonResult byUrl() {
+        return new CommonResult("按url限流", 200);
+    }
+
+    public CommonResult handleException(BlockException exception){
+        return new CommonResult(exception.getClass().getCanonicalName(),200);
+    }
+
+}
