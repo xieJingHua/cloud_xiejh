48f8869d1d5ffceae77939301084c0b79b201cdf
xieJingHua <xiejh888@gmail.com>
2020-10-25 21:14:07 +0800

    udpate seata分布式事务

---
+++ seate-order-service/src/main/java/com/xiejh/seateorderservice/SeateOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xiejh.seateorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeateOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeateOrderServiceApplication.class, args);
+    }
+
+}
